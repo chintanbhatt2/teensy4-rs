64dc036ac4a1e0f90fef3c3a34833fe74e926425
Chintan Bhatt <chintanbhatt2@gmail.com>
2025-09-22 11:46:21 -0700

    Adds support for FlexCAN

--- src/board.rs
+++ src/board.rs
@@ -136,6 +136,7 @@
 use crate::{hal, pins, ral};
 use core::sync::atomic::{AtomicBool, Ordering};
 pub use hal::lpspi::Pins as LpspiPins;
+pub use hal::can::Pins as CanPins;
 
 /// Use [`instances()`] to safely acquire.
 pub use ral::Instances;
@@ -277,6 +278,10 @@ pub struct Resources<Pins> {
     pub flexio2: ral::flexio::FLEXIO2,
     /// The FlexIO3 register block.
     pub flexio3: ral::flexio::FLEXIO3,
+        /// The FlexCAN1 register block.
+    pub flexcan1: ral::can::CAN1,
+    /// The FlexCAN1 register block.
+    pub flexcan2: ral::can::CAN2,
     /// The register block for ADC1.
     ///
     /// ADC drivers constructed by `board` use a pre-configured clock and divisor. To change
@@ -369,6 +374,41 @@ pub type Lpi2c1 = hal::lpi2c::Lpi2c<hal::lpi2c::Pins<pins::common::P19, pins::co
 /// Use [`lpi2c`] to create this driver.
 pub type Lpi2c3 = hal::lpi2c::Lpi2c<hal::lpi2c::Pins<pins::common::P16, pins::common::P17>, 3>;
 
+/// FlexCAN peripheral
+pub fn flexcan<Tx, Rx, const N: u8>(
+    instance: ral::can::Instance<N>,
+    tx: Tx,
+    rx: Rx,
+) -> hal::can::CAN<CanPins<Tx, Rx>, N>
+where
+    Tx: hal::iomuxc::flexcan::Pin<
+        Signal = hal::iomuxc::flexcan::Tx,
+        Module = hal::iomuxc::consts::Const<N>,
+    >,
+    Rx: hal::iomuxc::flexcan::Pin<
+        Signal = hal::iomuxc::flexcan::Rx,
+        Module = hal::iomuxc::consts::Const<N>,
+    >,
+{
+    hal::can::CAN::new(instance, tx, rx, CAN_FREQUENCY)
+}
+
+/// FlexCAN1 peripheral
+///
+/// - Pin 22 is TX
+/// - Pin 23 is RX
+///
+/// use [`flexcan`] to create this driver.
+pub type Flexcan1 = hal::can::CAN<CanPins<pins::common::P22, pins::common::P23>, 1>;
+
+/// FlexCAN2 peripheral
+///
+/// - Pin 1 is TX
+/// - Pin 0 is RX
+///
+/// use [`flexcan`] to create this driver.
+pub type Flexcan2 = hal::can::CAN<CanPins<pins::common::P1, pins::common::P0>, 2>;
+
 /// LPSPI1 peripheral.
 ///
 /// - SDO:  GPIO_SD_B0_02 (p43) or GPIO_EMC_28 (p50)
@@ -377,7 +417,7 @@ pub type Lpi2c3 = hal::lpi2c::Lpi2c<hal::lpi2c::Pins<pins::common::P16, pins::co
 /// - PCS0: GPIO_SD_B0_01 (p44) or GPIO_EMC_30
 ///
 /// Use [`lpspi`] to create this driver.
-pub type Lpspi1<SDO, SDI, SCK, PCS0> = hal::lpspi::Lpspi<LpspiPins<SDO, SDI, SCK, PCS0>, 1>;
+pub type Lpspi1<SDO, SDI, SCK> = hal::lpspi::Lpspi<LpspiPins<SDO, SDI, SCK>, 1>;
 
 /// LPSPI2 peripheral.
 ///
@@ -387,7 +427,7 @@ pub type Lpspi1<SDO, SDI, SCK, PCS0> = hal::lpspi::Lpspi<LpspiPins<SDO, SDI, SCK
 /// - PCS0: GPIO_SD_B1_06 or GPIO_EMC_01
 ///
 /// Use [`lpspi`] to create this driver.
-pub type Lpspi2<SDO, SDI, SCK, PCS0> = hal::lpspi::Lpspi<LpspiPins<SDO, SDI, SCK, PCS0>, 2>;
+pub type Lpspi2<SDO, SDI, SCK, > = hal::lpspi::Lpspi<LpspiPins<SDO, SDI, SCK, >, 2>;
 
 /// LPSPI3 peripheral.
 ///
@@ -399,8 +439,8 @@ pub type Lpspi2<SDO, SDI, SCK, PCS0> = hal::lpspi::Lpspi<LpspiPins<SDO, SDI, SCK
 /// - Pin 0 or 38 is chip select (CS).
 ///
 /// Use [`lpspi`] to create this driver.
-pub type Lpspi3<SDI, CS> =
-    hal::lpspi::Lpspi<LpspiPins<pins::common::P26, SDI, pins::common::P27, CS>, 3>;
+pub type Lpspi3<SDI> =
+    hal::lpspi::Lpspi<LpspiPins<pins::common::P26, SDI, pins::common::P27, >, 3>;
 
 /// LPSPI4 peripheral.
 ///
@@ -411,7 +451,7 @@ pub type Lpspi3<SDI, CS> =
 ///
 /// Use [`lpspi`] to create this driver.
 pub type Lpspi4 = hal::lpspi::Lpspi<
-    LpspiPins<pins::common::P11, pins::common::P12, pins::common::P13, pins::common::P10>,
+    LpspiPins<pins::common::P11, pins::common::P12, pins::common::P13,>,
     4,
 >;
 
@@ -440,11 +480,11 @@ pub type Lpspi4 = hal::lpspi::Lpspi<
 ///     1_000_000,
 /// );
 /// ```
-pub fn lpspi<Sdo, Sdi, Sck, Pcs0, const N: u8>(
+pub fn lpspi<Sdo, Sdi, Sck, const N: u8>(
     instance: ral::lpspi::Instance<N>,
-    pins: LpspiPins<Sdo, Sdi, Sck, Pcs0>,
+    pins: LpspiPins<Sdo, Sdi, Sck>,
     baud: u32,
-) -> hal::lpspi::Lpspi<LpspiPins<Sdo, Sdi, Sck, Pcs0>, N>
+) -> hal::lpspi::Lpspi<LpspiPins<Sdo, Sdi, Sck>, N>
 where
     Sdo: hal::iomuxc::lpspi::Pin<
         Signal = hal::iomuxc::lpspi::Sdo,
@@ -456,10 +496,6 @@ where
     >,
     Sck: hal::iomuxc::lpspi::Pin<
         Signal = hal::iomuxc::lpspi::Sck,
-        Module = hal::iomuxc::consts::Const<N>,
-    >,
-    Pcs0: hal::iomuxc::lpspi::Pin<
-        Signal = hal::iomuxc::lpspi::Pcs0,
         Module = hal::iomuxc::consts::Const<N>,
     >,
 {
@@ -671,6 +707,8 @@ fn prepare_resources<Pins>(
         flexpwm2: hal::flexpwm::new(instances.PWM2),
         flexpwm3: hal::flexpwm::new(instances.PWM3),
         flexpwm4: hal::flexpwm::new(instances.PWM4),
+        flexcan1: instances.CAN1,
+        flexcan2: instances.CAN2,
         adc1,
         adc2,
         trng,
